9ecfe73439b5d0b67fd0d853bdbc360376f27cce
zPG0D <pedromarininave123@gmail.com>
2022-07-16 16:26:23 -0300

    Changes

--- src/main/java/com/zpedroo/mcmmoaddon/listeners/DisabledSkillsListeners.java
+++ src/main/java/com/zpedroo/mcmmoaddon/listeners/DisabledSkillsListeners.java
@@ -9,41 +9,42 @@ import com.gmail.nossr50.events.skills.repair.McMMOPlayerRepairCheckEvent;
 import com.gmail.nossr50.events.skills.unarmed.McMMOPlayerDisarmEvent;
 import com.zpedroo.mcmmoaddon.utils.config.DisabledSkills;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class DisabledSkillsListeners implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onActivate(McMMOPlayerAbilityActivateEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onXpGain(McMMOPlayerXpGainEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onRepair(McMMOPlayerRepairCheckEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onDisarm(McMMOPlayerDisarmEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onFishing(McMMOPlayerFishingEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onFishingTreasure(McMMOPlayerFishingTreasureEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onCatalysis(McMMOPlayerCatalysisEvent event) {
         if (DisabledSkills.NAMES.contains(event.getSkill().name())) event.setCancelled(true);
     }
